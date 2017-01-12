9ca4bad8f1fdf6a7cbe565de39e0af03c605f6ee
sorrycc <sorrycc@gmail.com>
2017-01-12 17:55:32 +0800

    0.3.2, remove debug console.log

--- src/index.js
+++ src/index.js
@@ -146,7 +146,6 @@ export default function ({ types:t }) {
       Program: {
         enter(path) {
           const { filename } = path.hub.file.opts;
-          console.log('DELETE===============', filename);
           delete cache[filename];
         },
       },
