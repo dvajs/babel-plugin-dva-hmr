26a8af30a08054526bdc8690cb3730b322fb0a58
Shane-Lewes Tenorio <sltenorio99@gmail.com>
2019-05-21 18:58:26 -0500

    Support projects built with Babel 7 (#13)
    * Support projects built with Babel 7
    
    * Go into one more path object layer

--- src/index.js
+++ src/index.js
@@ -184,8 +184,9 @@ export default function ({ types:t }) {
           delete cache[filename];
         },
       },
-      CallExpression(path, { opts }) {
-        const { filename } = path.hub.file.opts;
+      CallExpression(path, state) {
+        const { opts } = state;
+        const {filename} = (path && path.hub && path.hub.file && path.hub.file.opts) || (state && state.file);
         if (cache[filename]) return;
         const { callee, arguments: args } = path.node;
         if (isRouterCall(callee, path.scope)) {
